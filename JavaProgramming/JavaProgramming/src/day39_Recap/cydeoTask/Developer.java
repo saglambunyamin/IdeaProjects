0b2f136ea0605e037cfd449898bbf1190bb8b95c
Bunyamin Saglam <saglambunyamin@gmail.com>
2022-12-16 16:50:36 +0300

    Cydeo Task completed.

---
+++ JavaProgramming/JavaProgramming/src/day39_Recap/cydeoTask/Developer.java
@@ -0,0 +1,19 @@
+package day39_Recap.cydeoTask;
+
+public class Developer extends Employee {
+
+    public Developer(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender, employeeID, jobTitle, salary);
+    }
+
+    public void fixingBugs(){
+        System.out.println(getJobTitle()+" "+getName()+" is fixing the bug");
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is developing the application");
+    }
+
+
+}
---
+++ JavaProgramming/JavaProgramming/src/day39_Recap/cydeoTask/Teacher.java
@@ -0,0 +1,14 @@
+package day39_Recap.cydeoTask;
+
+public class Teacher extends Employee{
+
+    public Teacher(String name, int age, char gender, int employeeID, String jobTitle, double salary) {
+        super(name, age, gender, employeeID, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle() + " " + getName() + " is teaching");
+    }
+
+}
