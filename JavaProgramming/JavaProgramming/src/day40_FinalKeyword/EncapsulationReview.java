5d7a205b29d0735ef65e12d3f308b6af99711d2b
Bunyamin Saglam <saglambunyamin@gmail.com>
2022-12-20 22:57:25 +0300

    Updated.

--- JavaProgramming/JavaProgramming/src/day40_FinalKeyword/EncapsulationReview.java
+++ JavaProgramming/JavaProgramming/src/day40_FinalKeyword/EncapsulationReview.java
@@ -9,10 +9,6 @@ public class EncapsulationReview {
         return c1;
     }
     public void setC1 (Circle c1) {
-        if (c1.setRadius() <= 0) {
-            System.out.println("Invalid Radius Value");
-            System.exit(0);
-        }
         this.c1 = c1;
     }
 
