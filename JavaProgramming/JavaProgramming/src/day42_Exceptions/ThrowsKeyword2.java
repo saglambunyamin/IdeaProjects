ce851017f5f6ec5424f2b18f53ad3a403299523b
Bunyamin Saglam <saglambunyamin@gmail.com>
2022-12-27 16:24:05 +0300

    Updated.

---
+++ JavaProgramming/JavaProgramming/src/day42_Exceptions/ThrowsKeyword2.java
@@ -0,0 +1,12 @@
+package day42_Exceptions;
+
+public class ThrowsKeyword2 {
+    public static void main(String[] args) {
+
+        System.out.println("Test Started");
+        System.out.println(8/0);
+        System.out.println("Test Completed");
+        
+
+    }
+}
