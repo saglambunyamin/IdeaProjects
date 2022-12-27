a5bbb26dc15462a061960e15875d62e44cf1dfd1
Bunyamin Saglam <saglambunyamin@gmail.com>
2022-12-27 22:50:56 +0300

    Updated.

---
+++ JavaProgramming/JavaProgramming/src/day43_Abstraction/Throw_vs_Throws.java
@@ -0,0 +1,34 @@
+package day43_Abstraction;
+
+import java.util.Scanner;
+
+public class Throw_vs_Throws {
+    public static void main(String[] args) {
+
+        System.out.println("Enter Your Score");
+        int score = new Scanner(System.in).nextInt();
+
+        if (score < 0 || score > 100) {
+            throw new RuntimeException("Invalid score: " + score);
+        }
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+
+    public static void method1 () throws InterruptedException {
+        System.out.println("Hello... Wait For It...");
+        Thread.sleep(5000);
+        System.out.println("Good Night....");
+    }
+
+
+}
