f9e609f3c8e9e988d7df38b19c3642234c2410e5
Bunyamin Saglam <saglambunyamin@gmail.com>
2023-01-03 14:20:13 +0300

    Updated.

--- JavaProgramming/JavaProgramming/src/day45_Abstraction/Shape/Shape.java
+++ JavaProgramming/JavaProgramming/src/day45_Abstraction/Shape/Shape.java
@@ -1,7 +1,27 @@
 package day45_Abstraction.Shape;
 
-public class Shape {
+public abstract class Shape {
 
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public Shape (String name) {
+        this.name = name;
+    }
+
+    public abstract double area();
+    public abstract double perimeter();
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "Name= '" + name + '\'' +
+                ", Area= '" + area() + '\'' +
+                ", Perimeter= '" + perimeter() + '\'' +
+                '}';
+    }
 
 }
